674aeb7b87c21428537914eea6bd84a7f1a5657c
Ali <Alimohammadnia127@gmail.com>
2024-11-17 02:53:29 +0330

    Stable

--- migrate_run.sh
+++ migrate_run.sh
@@ -5,4 +5,4 @@ python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
 python manage.py runserver 0.0.0.0:8000 
-#gunicorn core.wsgi:application --workers 3 --bind 0.0.0.0:8000
+gunicorn core.wsgi:application --workers 3 --bind 0.0.0.0:8000
